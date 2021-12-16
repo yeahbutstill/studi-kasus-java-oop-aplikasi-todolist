9f1b2b5b3b4748ab3e58ca7d7f31fea3a1306824
Dani Setiawan <mayadani28@gmail.com>
2021-12-16 08:50:32 +0700

    Created View

---
+++ src/com/yeahbutstill/view/TodoListView.java
@@ -0,0 +1,15 @@
+package com.yeahbutstill.view;
+
+public class TodoListView {
+    public void showTodoList(){
+
+    }
+
+    public void addTodoList(){
+
+    }
+
+    public void removeTodoList() {
+
+    }
+}
